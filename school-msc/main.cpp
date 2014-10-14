440b5b96170a0ea93a9c26dbe65810a2e87c95a8
Vlad <alzwded@gmail.com>
2014-10-14 11:14:31 +0300

    cleanup!

--- school-msc/main.cpp
+++ school-msc/main.cpp
@@ -5,15 +5,18 @@
 #include <cmath>
 #include <ctime>
 
+// our value type in the DB
 typedef struct {
     float v;
 } data_t;
 
+// our input type
 typedef struct {
     float k1;
     float k2;
 } inputPair_t;
 
+// our metadata container
 typedef struct {
     float k1_base, k1_n, k1_step;
     float k2_base, k2_n, k2_step;
@@ -23,15 +26,13 @@ int main(int argc, char* argv[])
 {
     if(argc != 2) return 255;
 
-    HANDLE hMapFile;
-    HANDLE hFile;
-    BOOL bFlag;
-    LPVOID lpMapAddress;
-    data_t* data;
+    // start timing
     clock_t start = clock();
+    // we'll substract I/O time later
     double timeInInput = 0.0;
 
-    hFile = CreateFileA(
+    // get a handle to our db file
+    HANDLE hFile = CreateFileA(
         "db.dat",
         GENERIC_READ,
         0,
@@ -39,14 +40,16 @@ int main(int argc, char* argv[])
         OPEN_EXISTING,
         FILE_ATTRIBUTE_NORMAL,
         NULL);
-    hMapFile = CreateFileMapping(
+    // map it
+    HANDLE hMapFile = CreateFileMapping(
         hFile,
         NULL,         
         PAGE_READONLY,
         0,            
         0,            
-        NULL);        
-    lpMapAddress = MapViewOfFile(
+        NULL);   
+    // view it
+    LPVOID lpMapAddress = MapViewOfFile(
         hMapFile,
         FILE_MAP_READ,
         0,
@@ -61,16 +64,22 @@ int main(int argc, char* argv[])
         &metadata.k1_base, &metadata.k1_n, &metadata.k1_step,
         &metadata.k2_base, &metadata.k2_n, &metadata.k2_step);
 
+    // init output file
     FILE* fout;
     (void) fopen_s(&fout, "output.txt", "w");
 
+    // reserve our inputs and outputs to save time later
     std::vector<float> outputs;
     outputs.reserve(10000);
     std::vector<inputPair_t> inputs;
     inputs.reserve(10000);
+
+    // read everything in one big chunk because we'd block
+    // on I/O if we intermix I/O and computations
     FILE* finputs;
     (void) fopen_s(&finputs, argv[1], "r");
     inputPair_t in;
+    // time the reading because it's awefully slow
     clock_t startInputClock = clock();
     while(!feof(finputs)) {
         auto hr = fscanf_s(finputs, "%f%f", &in.k1, &in.k2);
@@ -79,20 +88,24 @@ int main(int argc, char* argv[])
         inputs.push_back(in);
     }
     clock_t endInputClock = clock();
+    // count I/O time
     timeInInput += endInputClock - startInputClock;
 
     // here's the data, do stuff with it
-    data = (data_t*)lpMapAddress;
+    data_t* data = (data_t*)lpMapAddress;
     
     long const nbOfInputs = inputs.size();
     inputPair_t* p = inputs.data();
     inputPair_t* pStart = p;
-    for(; p - pStart < nbOfInputs; ++p) {
+    inputPair_t* pEnd = pStart + nbOfInputs;
+    for(; p < pEnd; ++p) {
         inputPair_t& in = *p;
 
+        // compute approximate indexes
         size_t k1idx = (size_t)( floorf((in.k1 - metadata.k1_base) / metadata.k1_step) );
         size_t k2idx = (size_t)( floorf((in.k2 - metadata.k2_base) / metadata.k2_step) );
         size_t idx = k1idx * sizeof(data_t) + k2idx;
+        // grab the result
         data_t datum = data[idx];
 
         // write results
@@ -102,30 +115,35 @@ int main(int argc, char* argv[])
     // stop timing because I/O
     clock_t end = clock();
 
+    // write output file
     for(size_t i = 0; i < outputs.size(); ++i)
     {
         fprintf_s(fout, "f(%5.2f, %5.2f) = %f\n", 
             inputs[i].k1, inputs[i].k2, outputs[i]);
     }
 
+    // report time
     double theTime = (double)(end - start) / CLOCKS_PER_SEC;
+    timeInInput /= CLOCKS_PER_SEC;
+
     printf("Computed %ld values in %ld ticks, or %lfs, timed using clock()\n",
         nbOfInputs,
         end - start,
         theTime);
 
-    timeInInput /= CLOCKS_PER_SEC;
-
     printf("Mind you, %lfs was spent in I/O operations on the %s file\n"
         "and thus, only %fs were spent actually computing stuff\n",
-        timeInInput, argv[1], theTime - timeInInput);
+        timeInInput, 
+        argv[1],
+        theTime - timeInInput);
     
+    // cleanup
     fclose(fout);
     fclose(fmetadata);
     fclose(finputs);
-    bFlag = UnmapViewOfFile(lpMapAddress);
-    bFlag = CloseHandle(hMapFile);
-    bFlag = CloseHandle(hFile);
+    (void) UnmapViewOfFile(lpMapAddress);
+    (void) CloseHandle(hMapFile);
+    (void) CloseHandle(hFile);
 
     return 0;
 }

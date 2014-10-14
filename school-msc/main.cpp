bb3e88cf81e5cf1379c55d3bf69303c00514f4e3
Vlad <alzwded@gmail.com>
2014-10-14 10:27:32 +0300

    cleanup file because visual studio

--- school-msc/main.cpp
+++ school-msc/main.cpp
@@ -5,8 +5,6 @@
 #include <cmath>
 #include <ctime>
 
-#define BUFFSIZE 1024 // size of the memory to examine at any one time
-
 typedef struct {
     float v;
 } data_t;
@@ -23,7 +21,7 @@ typedef struct {
 
 int main(int argc, char* argv[])
 {
-	if(argc != 2) return 255;
+    if(argc != 2) return 255;
 
     HANDLE hMapFile;
     HANDLE hFile;
@@ -31,7 +29,7 @@ int main(int argc, char* argv[])
     LPVOID lpMapAddress;
     data_t* data;
     clock_t start = clock();
-	double timeInInput = 0.0;
+    double timeInInput = 0.0;
 
     hFile = CreateFileA(
         "db.dat",
@@ -66,31 +64,31 @@ int main(int argc, char* argv[])
     FILE* fout;
     (void) fopen_s(&fout, "output.txt", "w");
 
-	std::vector<float> outputs;
-	outputs.reserve(10000);
-	std::vector<inputPair_t> inputs;
-	inputs.reserve(10000);
+    std::vector<float> outputs;
+    outputs.reserve(10000);
+    std::vector<inputPair_t> inputs;
+    inputs.reserve(10000);
     FILE* finputs;
     (void) fopen_s(&finputs, argv[1], "r");
     inputPair_t in;
-	clock_t startInputClock = clock();
-	while(!feof(finputs)) {
-		auto hr = fscanf_s(finputs, "%f%f", &in.k1, &in.k2);
-		if(hr != 2) break;
+    clock_t startInputClock = clock();
+    while(!feof(finputs)) {
+        auto hr = fscanf_s(finputs, "%f%f", &in.k1, &in.k2);
+        if(hr != 2) break;
 
-		inputs.push_back(in);
-	}
-	clock_t endInputClock = clock();
-	timeInInput += endInputClock - startInputClock;
+        inputs.push_back(in);
+    }
+    clock_t endInputClock = clock();
+    timeInInput += endInputClock - startInputClock;
 
     // here's the data, do stuff with it
     data = (data_t*)lpMapAddress;
-	
-	long const nbOfInputs = inputs.size();
-	inputPair_t* p = inputs.data();
-	inputPair_t* pStart = p;
-	for(; p - pStart < nbOfInputs; ++p) {
-		inputPair_t& in = *p;
+    
+    long const nbOfInputs = inputs.size();
+    inputPair_t* p = inputs.data();
+    inputPair_t* pStart = p;
+    for(; p - pStart < nbOfInputs; ++p) {
+        inputPair_t& in = *p;
 
         size_t k1idx = (size_t)( floorf((in.k1 - metadata.k1_base) / metadata.k1_step) );
         size_t k2idx = (size_t)( floorf((in.k2 - metadata.k2_base) / metadata.k2_step) );
@@ -98,29 +96,29 @@ int main(int argc, char* argv[])
         data_t datum = data[idx];
 
         // write results
-		outputs.push_back(datum.v);
+        outputs.push_back(datum.v);
     }
 
-	// stop timing because I/O
+    // stop timing because I/O
     clock_t end = clock();
 
-	for(size_t i = 0; i < outputs.size(); ++i)
-	{
+    for(size_t i = 0; i < outputs.size(); ++i)
+    {
         fprintf_s(fout, "f(%5.2f, %5.2f) = %f\n", 
-			inputs[i].k1, inputs[i].k2, outputs[i]);
-	}
+            inputs[i].k1, inputs[i].k2, outputs[i]);
+    }
 
-	double theTime = (double)(end - start) / CLOCKS_PER_SEC;
+    double theTime = (double)(end - start) / CLOCKS_PER_SEC;
     printf("Computed %ld values in %ld ticks, or %lfs, timed using clock()\n",
         nbOfInputs,
         end - start,
         theTime);
 
-	timeInInput /= CLOCKS_PER_SEC;
+    timeInInput /= CLOCKS_PER_SEC;
 
-	printf("Mind you, %lfs was spent in I/O operations on the %s file\n"
-		"and thus, only %fs were spent actually computing stuff\n",
-		timeInInput, argv[1], theTime - timeInInput);
+    printf("Mind you, %lfs was spent in I/O operations on the %s file\n"
+        "and thus, only %fs were spent actually computing stuff\n",
+        timeInInput, argv[1], theTime - timeInInput);
     
     fclose(fout);
     fclose(fmetadata);
